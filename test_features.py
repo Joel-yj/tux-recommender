b90afada1802027985852c3727082a68120089ac
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-10-23 23:45:01 +0800

    push feature objects into weaviate and query

--- test_features.py
+++ test_features.py
@@ -1,63 +1,58 @@
 import weaviate
-import pandas as pd
-from weaviate.util import generate_uuid5
+import json
 
 
-client = weaviate.Client('http://localhost:8080')
+client = weaviate.Client("http://localhost:8080")
 
-client.schema.delete_class('Features')
-client.schema.delete_class('Distribution')
-client.schema.delete_class('Distributions')
+# Settings for displaying the import progress
+counter = 0
+interval = 20  # print progress every this many records; should be bigger than the batch_size
 
-class_obj = {
-    "class": "arch",
-    "description": "A class to implement distribution descriptions",
-    "moduleConfig": {
-        "text2vec-transformers": {
-            "vectorizeClassName": "false"
-        }
-    },
-    "properties": [{
-        "dataType": ["string"],
-        "name": "name1",
-        "moduleConfig": {
-            "text2vec-transformers": {
-                "skip": "true"   
-            }
-        },
-        "description": "feature of arch"
-    },
-    {
-        "dataType": ["string"],
-        "name": "name2",
-        "moduleConfig": {
-            "text2vec-transformers": {
-                "skip": "false"  # only vectorize description so that later search is done between descriptions
-            }
-        },
-        "description": "release date"
-    }]
-}
+def add_debian_release(release_name, release_data):
+    properties = {
+        'Release_Date': release_data['Release Date'],
+        'End_Of_Life': release_data['End Of Life'],
+        'Price': release_data['Price (US$)'],
+        'Image_Size': release_data['Image Size (MB)'],
+        'Free_Download': release_data['Free Download'],
+        'Installation': release_data['Installation'],
+        'Default_Desktop': release_data['Default Desktop'],
+        'Package_Management': release_data['Package Management'],
+        'Release_Model': release_data['Release Model'],
+        'Office_Suite': release_data['Office Suite'],
+        'Processor_Architecture': release_data['Processor Architecture'],
+        'Init_Software': release_data['Init Software'],
+        'Journaled_File_Systems': release_data['Journaled File Systems'],
+        'Multilingual': release_data['Multilingual'],
+        'Asian_Language_Support': release_data['Asian Language Support'],
+        'Number_of_Packages': release_data['Number of Packages'],
+    }
 
-data = pd.read_json('data/arch.json')
-class_name = "arch"
+    client.batch.configure(batch_size=100)  # Configure batch
+    with client.batch as batch:
+        # Add the object to the batch
+        batch.add_data_object(
+            data_object=properties,
+            class_name='Debian',
+            # If you Bring Your Own Vectors, add the `vector` parameter here
+            # vector=obj.vector
+        )
 
-data_objs = []
-for i,row in data.iterrows():
-    obj = {"name1": data['Feature'][i],
-     "name2": data['Free Download'][i]}
-    data_objs.append(obj)
 
-# client.batch.configure(batch_size=100)
+with open('data/debian.json', 'r') as json_file:
+    data = json.load(json_file)
+    for release_name, release_data in data.items():
+        add_debian_release(release_name, release_data)
 
-with client.batch as batch:
-    for data_obj in data_objs:
-        try:
-            batch.add_data_object(
-                data_obj,
-                class_name,
-                uuid=generate_uuid5(data_obj)
-            )
-            print(f"Added object with name: {data_obj['name2']}")
-        except Exception as e:
-            print(f"Error adding object with name {data_obj['name2']}. Error: {e}")
+print('Importing Debian releases into Weaviate...')
+client.batch.flush()
+print('Finished importing Debian releases.')
+
+
+response = (
+    client.query
+    .get("Debian", ["Release Date"])
+    .with_limit(1)
+    .do()
+)
+print(response)
--- test_object.py
+++ test_object.py
@@ -4,9 +4,9 @@ client = weaviate.Client("http://localhost:8080")
 
 response = (
     client.query
-    .get("arch",["name"])
+    .get("Debian", ["price"])
+    .with_limit(1)
     .do()
 )
-
 print(response)
 
